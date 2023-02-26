ec5a33f369383f0bcaed0b47b806f5217664c790
Rupesh yadav <rupeshyadav94.ryy@gmail.com>
2023-02-26 23:44:43 +0530

    Add Welcome API

--- src/api/config/environment.js
+++ src/api/config/environment.js
@@ -5,21 +5,21 @@ const Environment = process.env.NODE_ENV;
 
 // Load env file
 if (fs.existsSync(path.join(process.cwd(), `/.env.${Environment}`))) {
-	dotenv.config({
-		path: `.env.${Environment}`,
-	});
+  dotenv.config({
+    path: `.env.${Environment}`,
+  });
 } else {
-	process.exit(1);
+  process.exit(1);
 }
 
 module.exports = (function () {
-	return {
-		PORT: process.env.PORT || 3000,
-		API_URI: process.env.API_URI,
-		DATABASE: process.env.DATABASE || 'mongodb://127.0.0.1:27017/test',
-		ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
-		REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
-		ACCESS_TOKEN_EXPIRATION: process.env.ACCESS_TOKEN_EXPIRATION,
-		REFRESH_TOKEN_EXPIRATION: process.env.REFRESH_TOKEN_EXPIRATION,
-	};
+  return {
+    PORT: process.env.PORT || 3000,
+    API_URI: process.env.API_URI,
+    DATABASE: process.env.DATABASE || 'mongodb://127.0.0.1:27017/test',
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+    ACCESS_TOKEN_EXPIRATION: process.env.ACCESS_TOKEN_EXPIRATION,
+    REFRESH_TOKEN_EXPIRATION: process.env.REFRESH_TOKEN_EXPIRATION,
+  };
 })();
--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 const user = require('./user.route');
 const auth = require('./auth.route');
 
+router.get('/', () => {
+  return res.status(200).json({ message: 'Welcome page' });
+});
+
 router.use('/auth', auth);
 router.use('/users', user);
 
